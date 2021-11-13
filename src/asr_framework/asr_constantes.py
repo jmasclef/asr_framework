69452128f354bdbc9379d66fd2e2096ea84bb796
Julien Masclef <julien.masclef@gmail.com>
2021-11-13 19:14:39 +0100

    Intégration progressive

--- src/asr_framework/asr_constantes.py
+++ src/asr_framework/asr_constantes.py
@@ -37,41 +37,7 @@ SEUIL_HAUT_SCORE_MOYEN=60
 SEUIL_HAUT_SCORE_BON=70
 SEUIL_HAUT_SCORE_TRES_BON=85
 
-"""
-SECTION PARAMETRES GENSIM
-"""
-intermediate_min_tf_word = 2  # min term freq for plural,accents and gensim
-intermediate_min_df_word = 2  # min doc freq for plural and accents
-final_min_tf_word = 3
-similarity_limit_for_replacement = 0.6  # 0.7 baissé à 0.6
-gensim_model_itermediate_iterations = 7  # default is 5 but can give up to 20% similarity variations
-gensim_model_final_iterations = 10
-"""
-Sur 5 itérations les scores de similarité ont de fortes variations d'un modèle à un autre, jusqu'à 20%...
-Passer à 10 stabilise les variations à l'ordre du 1%
-Passer de 10 à 15 réduit les scores de quelques pourcents (4% constatés)
-"""
-gensim_model_dimension = 300  # (100-1000) 300 is known as a good value, cf standford
-gensim_model_window = 10  # Maximum distance between the current and predicted word within a sentence
-"""
-The size of the context window determines how many words before and after a given word would be included as context
-words of the given word. According to the authors' note, the recommended value is 10 for skip-gram and 5 for CBOW.
-=> Faut il tester avec moins que 10 en supposant que les descriptions professionnelles sont plus condensées et moins rédigées ?
-"""
-gensim_training_algo_skipgram = 1
-gensim_training_algo_cbow = 0
-gensim_training_algo = gensim_training_algo_skipgram  # Training algorithm: 1 for skip-gram; otherwise CBOW
-"""  
-The CBOW architecture predicts the current word based on the context.
-The order of context words does not influence prediction (bag-of-words assumption). 
-In the continuous skip-gram architecture, the model uses the current word to predict the surrounding window of context 
-words. The skip-gram architecture weighs nearby context words more heavily than more distant context words.
-Finally, the Skip-gram architecture works slightly worse on the syntactic task than the CBOW model, and much better on
-the semantic part of the test than all the other models. 
-CBOW éffondre les scores de similarité
-CBOW is faster while skip-gram is slower but does a better job for infrequent words.
 
-"""
 
 """
 SECTION CONSTANTES ANALYSE
--- src/asr_framework/asr_gensimCustom.py
+++ src/asr_framework/asr_gensimCustom.py
@@ -1,5 +1,40 @@
 import gensim
 
+"""
+SECTION PARAMETRES GENSIM
+"""
+intermediate_min_tf_word = 2  # min term freq for plural,accents and gensim
+intermediate_min_df_word = 2  # min doc freq for plural and accents
+final_min_tf_word = 3
+similarity_limit_for_replacement = 0.6  # 0.7 baissé à 0.6
+gensim_model_itermediate_iterations = 7  # default is 5 but can give up to 20% similarity variations
+gensim_model_final_iterations = 10
+"""
+Sur 5 itérations les scores de similarité ont de fortes variations d'un modèle à un autre, jusqu'à 20%...
+Passer à 10 stabilise les variations à l'ordre du 1%
+Passer de 10 à 15 réduit les scores de quelques pourcents (4% constatés)
+"""
+gensim_model_dimension = 300  # (100-1000) 300 is known as a good value, cf standford
+gensim_model_window = 10  # Maximum distance between the current and predicted word within a sentence
+"""
+The size of the context window determines how many words before and after a given word would be included as context
+words of the given word. According to the authors' note, the recommended value is 10 for skip-gram and 5 for CBOW.
+=> Faut il tester avec moins que 10 en supposant que les descriptions professionnelles sont plus condensées et moins rédigées ?
+"""
+gensim_training_algo_skipgram = 1
+gensim_training_algo_cbow = 0
+gensim_training_algo = gensim_training_algo_skipgram  # Training algorithm: 1 for skip-gram; otherwise CBOW
+"""  
+The CBOW architecture predicts the current word based on the context.
+The order of context words does not influence prediction (bag-of-words assumption). 
+In the continuous skip-gram architecture, the model uses the current word to predict the surrounding window of context 
+words. The skip-gram architecture weighs nearby context words more heavily than more distant context words.
+Finally, the Skip-gram architecture works slightly worse on the syntactic task than the CBOW model, and much better on
+the semantic part of the test than all the other models. 
+CBOW éffondre les scores de similarité
+CBOW is faster while skip-gram is slower but does a better job for infrequent words.
+"""
+
 class asr_virtual_model():
     def __init__(self,gensim_model: gensim.models.Word2Vec =None,gensim_wv: gensim.models.KeyedVectors =None):
         if gensim_model:
@@ -41,3 +76,8 @@ def asr_save_light_model(*,model_name:gensim.models.Word2Vec,model_filename:str)
 def asr_load_light_model(*, model_filename: str):
     wv = gensim.models.KeyedVectors.load(fname=model_filename,mmap='r')
     return asr_virtual_model(gensim_wv=wv)
+
+def asr_gensim_word2vec(corpus, size=gensim_model_dimension, sg=gensim_training_algo, window=gensim_model_window,
+                                       min_count=intermediate_min_tf_word, iter=gensim_model_itermediate_iterations):
+    gensim.models.Word2Vec(corpus, size=size, sg=sg, window=window, min_count=min_count,iter=iter,
+                           workers=os.cpu_count(), trim_rule=None)
