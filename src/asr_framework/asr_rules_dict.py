caff4a57c0c70a7ef48d01941f2b452d9a454d8f
Julien <julien.masclef@gmail.com>
2021-12-16 16:47:00 +0100

    ADD GNU GPL v3

--- src/asr_framework/asr_rules_dict.py
+++ src/asr_framework/asr_rules_dict.py
@@ -6,7 +6,7 @@
     you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    any later version.
 
     Framework Assistance Sémantique au Recrutement (ASR) is distributed
     in the hope that it will be useful,
